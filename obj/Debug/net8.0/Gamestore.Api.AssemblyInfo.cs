74cdbff6110edb91baad3686a90179b81a2f4a32
Mani Movassagh <mani.mgh@gmail.com>
2024-07-14 01:35:38 +0200

    hot reloader

--- obj/Debug/net8.0/Gamestore.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gamestore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf9688cdb76132bb1d1a7a926138d146a5b15e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e757e6f7f93179c9bd205617d6ef258692355c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
