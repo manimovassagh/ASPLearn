c3e757e6f7f93179c9bd205617d6ef258692355c
Mani Movassagh <mani.mgh@gmail.com>
2024-07-14 01:31:04 +0200

    json

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/json/{name}", (string name) => Results.Json(new { Name = name }));
 
 app.Run();
