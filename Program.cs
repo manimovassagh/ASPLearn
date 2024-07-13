830ec7d19995fcea3cd5f4e398e169fcf7f3b35e
Mani Movassagh <mani.mgh@gmail.com>
2024-07-14 01:57:04 +0200

    continue

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var res = new { Namer = "Mani" };
 
-
-app.MapGet("/json/{name}", (string name) => Results.Json(new { Namer = name }));
+app.MapGet("/json/{name}", (string name) => Results.Json(res));
 app.MapGet("/", () => Results.Json(new { Namer = "App is healthy and running" }));
 
 app.Run();
