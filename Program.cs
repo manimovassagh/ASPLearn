3a49f5e6c13ee4daaa70e0d5b65dd60c570455a0
Mani Movassagh <mani.mgh@gmail.com>
2024-07-14 23:18:37 +0200

    test

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
-
 app.MapGamingEndpoints();
 
 app.Run();
--- controllers/Endpoints.cs
+++ controllers/Endpoints.cs
@@ -1,18 +1,17 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using ASPLearn.Entities;
 
 namespace ASPLearn
 {
-    public static class GamingEndpoints
-    {
-        public static void MapGamingEndpoints(this WebApplication app)
+        public static class GamingEndpoints
         {
-            var res = new { Name = "Mani" };
+            public static void MapGamingEndpoints(this WebApplication app)
+            {
+                var res = new { Name = "Mani" };
 
-            app.MapGet("/json/{name}", (string name) => Results.Json(res));
-            app.MapGet("/", () => new Gaming().Games);
-            app.MapGet("/sahar", () => new Gaming().Games);
+                app.MapGet("/json/{name}", (string name) => Results.Json(res));
+                app.MapGet("/", () => new Gaming().Games);
+                app.MapGet("/sahar", () => new Gaming().Games);
+            }
         }
-    }
+
 }
